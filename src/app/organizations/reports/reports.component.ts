f39e2b150fabc4af40cad8fe1d96b1951cce7518
gisselac <gisselac@hitachi-id.com>
2022-02-01 20:15:02 -0500

    Run npm run prettier.

--- src/app/organizations/reports/reports.component.ts
+++ src/app/organizations/reports/reports.component.ts
@@ -1,34 +1,37 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
-import { Organization } from 'jslib-common/models/domain/organization';
+import { Organization } from "jslib-common/models/domain/organization";
 
-import { MessagingService } from 'jslib-common/abstractions/messaging.service';
-import { UserService } from 'jslib-common/abstractions/user.service';
+import { MessagingService } from "jslib-common/abstractions/messaging.service";
+import { UserService } from "jslib-common/abstractions/user.service";
 
 @Component({
-    selector: 'app-org-reports',
-    templateUrl: 'reports.component.html',
+  selector: "app-org-reports",
+  templateUrl: "reports.component.html",
 })
 export class ReportsComponent {
-    organization: Organization;
-    accessReports = false;
-    loading = true;
+  organization: Organization;
+  accessReports = false;
+  loading = true;
 
-    constructor(private route: ActivatedRoute, private userService: UserService,
-        private messagingService: MessagingService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService,
+    private messagingService: MessagingService
+  ) {}
 
-    ngOnInit() {
-        this.route.parent.params.subscribe(async params => {
-            this.organization = await this.userService.getOrganization(params.organizationId);
-            // TODO: Maybe we want to just make sure they are not on a free plan? Just compare useTotp for now
-            // since all paid plans include useTotp
-            this.accessReports = this.organization.useTotp;
-            this.loading = false;
-        });
-    }
+  ngOnInit() {
+    this.route.parent.params.subscribe(async (params) => {
+      this.organization = await this.userService.getOrganization(params.organizationId);
+      // TODO: Maybe we want to just make sure they are not on a free plan? Just compare useTotp for now
+      // since all paid plans include useTotp
+      this.accessReports = this.organization.useTotp;
+      this.loading = false;
+    });
+  }
 
-    upgradeOrganization() {
-        this.messagingService.send('upgradeOrganization', { organizationId: this.organization.id });
-    }
+  upgradeOrganization() {
+    this.messagingService.send("upgradeOrganization", { organizationId: this.organization.id });
+  }
 }
--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -1,28 +1,25 @@
-import {
-    Component,
-    OnInit,
-} from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { MessagingService } from 'jslib-common/abstractions/messaging.service';
-import { UserService } from 'jslib-common/abstractions/user.service';
+import { MessagingService } from "jslib-common/abstractions/messaging.service";
+import { UserService } from "jslib-common/abstractions/user.service";
 
 @Component({
-    selector: 'app-reports',
-    templateUrl: 'reports.component.html',
+  selector: "app-reports",
+  templateUrl: "reports.component.html",
 })
 export class ReportsComponent implements OnInit {
-    canAccessPremium = false;
+  canAccessPremium = false;
 
-    constructor(private userService: UserService, private messagingService: MessagingService) { }
+  constructor(private userService: UserService, private messagingService: MessagingService) {}
 
-    async ngOnInit() {
-        this.canAccessPremium = await this.userService.canAccessPremium();
+  async ngOnInit() {
+    this.canAccessPremium = await this.userService.canAccessPremium();
+  }
+
+  premiumRequired() {
+    if (!this.canAccessPremium) {
+      this.messagingService.send("premiumRequired");
+      return;
     }
-
-    premiumRequired() {
-        if (!this.canAccessPremium) {
-            this.messagingService.send('premiumRequired');
-            return;
-        }
-    }
+  }
 }
